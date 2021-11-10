5597a9bb2c338afccaab5d13356d3fbc0dcdc246
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2021-11-10 11:53:09 -0700

    redux store established

---
+++ src/store/mainApi.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const mainApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://kmin2684.github.io/mock-api/oh_fe_test/' }),
+  tagTypes: [],
+  endpoints: (builder) => ({
+    getCommunities: builder.query<any[], string>({
+      query: () => `communities.json`,
+    }),
+    getHouses: builder.query({
+      query: () => 'homes.json',
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components
+export const { useGetCommunitiesQuery,useGetHousesQuery } = mainApi
